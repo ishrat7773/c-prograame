9889e3b9b7c07d4c2fd76f76b0a51143cb0cd390
ishrat7773 <126759682+ishrat7773@users.noreply.github.com>
2023-04-06 12:50:48 +0600

    Update 2764 SOLUTION IN C.c

--- 2764 SOLUTION IN C.c
+++ 2764 SOLUTION IN C.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+
+     int A,B,C;
+
+    scanf("%d/" "%d/" "%d", &A, &B, &C);
+
+    printf("%.2d/%.2d/%.2d\n", B, A, C);
+
+    printf("%.2d/%.2d/%.2d\n", C, B, A);
+
+    printf("%.2d-%.2d-%.2d\n", A, B, C);
+
+    return 0;
+}
 
